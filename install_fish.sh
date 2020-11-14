4370e28a5e2251d8837bdafabbb519fdefc8197e
Andy Roulin <aroulin@nvidia.com>
2020-11-14 08:24:14 +0000

    guard install fish

--- install_fish.sh
+++ install_fish.sh
@@ -1,8 +1,25 @@
-#/bin/bash
+#!/bin/bash
 
-apt-get install fish
+if [[ $1 == "theme" ]]; then
+	omf install budspencer
+	omf theme budspencer
+	omf reload
+	exit 0
+fi
+
+read -p "Ok to be root? [yN]" yn
+
+if [[ "$yn" == "y" ]]; then
+	if [ ! grep "^deb     http://deb.debian.org/debian buster main" /etc/apt/sources.list] ; then
+		echo "deb     http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
+ 	fi	
+
+	sudo apt-get update
+	sudo apt-get install fish
+else
+	exit 0
+fi
 
 curl -L https://get.oh-my.fish | fish
-omf install budspencer
-omf theme budspencer
-omf reload
+
+
